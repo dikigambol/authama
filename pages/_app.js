697f7d80f7558be57ea5ae607c7baf223f9317d9
Diki Akbar <57055952+dikigambol@users.noreply.github.com>
2024-03-01 13:17:38 +0700

    add icon

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function App({ Component, pageProps }) {
                 <title>Authama</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="description" content="Authama is a platform revolutionizing your digital efforts with our state-of-the-art blockchain authentication solution. Seamlessly verify and secure the identities of goods and transactions using blockchain technology, ensuring unparalleled transparency, integrity, and trust." />
-                <link rel="icon" type="image/svg+xml" href="" />
+                <link rel="icon" type="image/svg+xml" href="/imgs/icon.png" />
                 <link href="/lib/fontawesome-free/css/all.min.css" rel="stylesheet" />
                 <link href="/lib/ionicons/css/ionicons.min.css" rel="stylesheet" />
                 <link href="/lib/typicons.font/typicons.css" rel="stylesheet" />
